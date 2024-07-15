3a5e3ae12714b20c4e5edb166dd315eeb252fac0
Pinocao <472730627@qq.com>
2024-07-15 16:52:35 +0800

    feat: 修改md配置 - 显示代码行数

--- docs/.vitepress/configs/markdown/index.ts
+++ docs/.vitepress/configs/markdown/index.ts
@@ -1,5 +1,6 @@
 import mdItCustomAttrs from "markdown-it-custom-attrs";
 export const markdown = {
+  lineNumbers: true,
   config: (md) => {
     // use more markdown-it plugins!
     md.use(mdItCustomAttrs, "image", {
